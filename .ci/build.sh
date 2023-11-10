5b16034ce898c5bd363093bbd24846fa6ba294bb
Robin Boers <robindev2019@outlook.com>
2023-11-10 21:49:18 +0100

    Add member directory

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env fish
 
+echo "=> Generating member directory"
+cp members.template members.txt
+
+for user in (getent group members | cut -d: -f4 | tr ',' \\n)
+  echo "- [~$user](/~$user)" >> members.txt
+end
+
+echo
+echo "=> Building site"
+
 ~/chop/chop.js
+
+echo
+echo "=> Formatting generated HTML"
+
+cd dist/web && prettier --write ./**/*.html
+
